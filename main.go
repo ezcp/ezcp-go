c0ad77c7ae79a6b3617b499a41caa44c08e90dce
chris <chris@xtralife.cloud>
2017-03-05 12:32:52 +0000

    done

--- main.go
+++ main.go
@@ -70,5 +70,80 @@ func main() {
 			}
 			return
 		}
+	case 1:
+		token, err := getDurableToken()
+		if err == nil && stdinTerminal() && stdoutTerminal() && !isSHA1Token(flag.Arg(0)) {
+			fpath := flag.Arg(0)
+			fileinfo, err := os.Stat(fpath)
+			if err == nil && !fileinfo.IsDir() {
+				// upload mode
+				file, err := os.Open(fpath)
+				if err != nil {
+					log.Fatal(err)
+				}
+				err = upload(*passphrase, file, token)
+				if err != nil {
+					log.Fatal(err)
+				}
+				return
+			}
+			// download mode
+			file, err := os.Create(fpath)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = download(*passphrase, file, token)
+			if err != nil {
+				log.Fatal(err)
+			}
+			return
+		}
+		token = flag.Arg(0)
+		if !isSHA1Token(token) {
+			log.Fatalf("%s is not a valid token", token)
+		}
+		if stdinTerminal() {
+			err := download(*passphrase, os.Stdout, token)
+			if err != nil {
+				log.Fatal(err)
+			}
+			return
+		}
+		err = upload(*passphrase, os.Stdin, token)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	case 2:
+		arg0 := flag.Arg(0)
+		arg1 := flag.Arg(1)
+		if isSHA1Token(arg0) && !isSHA1Token(arg1) {
+			fpath := arg1
+			token := arg0
+			file, err := os.Create(fpath)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = download(*passphrase, file, token)
+			if err != nil {
+				log.Fatal(err)
+			}
+			return
+		}
+		if !isSHA1Token(arg0) || isSHA1Token(arg1) {
+			fpath := arg0
+			token := arg1
+			file, err := os.Open(fpath)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = upload(*passphrase, file, token)
+			if err != nil {
+				log.Fatal(err)
+			}
+			return
+		}
+		log.Fatal("No recognized token on command line")
 	}
 }
